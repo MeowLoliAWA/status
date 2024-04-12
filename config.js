62d1383f5d94aa58d5341d63f378d79745115d41
MeowLoli <126253767+MeowLoliAWA@users.noreply.github.com>
2024-04-12 23:56:02 +0800

    status

---
+++ config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2521063-832dfbc74bcb5fafc9055cb6',
+    'ur2521063-f92d6ed834678c823d94c389',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: ''
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/MeowLoliAWA/status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.sakimeow.moe'
+    },
+  ],
+};
